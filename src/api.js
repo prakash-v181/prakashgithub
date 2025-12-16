97aa2101bb843ede09a11ce16393e0aa97538f5f
prakash-v181 <prakashtare110@outlook.com>
2025-12-16 17:05:54 +0530

    api update

--- src/api.js
+++ src/api.js
@@ -1,18 +1,16 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3002/api",
+  baseURL: "https://backendgithub-uz08.onrender.com/api",
+  withCredentials: true,
 });
 
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default api;
